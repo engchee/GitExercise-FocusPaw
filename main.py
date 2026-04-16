dcb3b05f8ffbd89bcbfbba94d2a663d33f134df5
Wong Eng Chee <wong.eng.chee@student.mmu.edu.my>
2026-04-16 12:15:28 +0800

    Edit main window coding.

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import tkinter as tk
+import tkinter.font as tKFont
 
 window = tk.Tk()
 window.title("FocusPaw")
+window.geometry("400x400")
 
-width = 400
-height = 400
+title_font = tKFont.Font(family="Courier", size=24, weight="bold", slant="italic")
+button_font = tKFont.Font(family="Arial", size=12, weight="bold")
+
+label = tk.Label (window, text="FocusPaw", font=title_font)
+label.pack()
+
+start_button = tk.Button(window, text="Start Timer", font=button_font)
+start_button.pack()
+
+window.mainloop()
 
 
 
