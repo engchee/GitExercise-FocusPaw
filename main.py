8daed7bf1aedb70a11897b85060f1adeb24eb5b8
Wong Eng Chee <wong.eng.chee@student.mmu.edu.my>
2026-04-16 12:30:40 +0800

    Change button size and position.

--- main.py
+++ main.py
@@ -3,16 +3,31 @@ import tkinter.font as tKFont
 
 window = tk.Tk()
 window.title("FocusPaw")
-window.geometry("400x400")
+window.geometry("500x500")
 
-title_font = tKFont.Font(family="Courier", size=24, weight="bold", slant="italic")
-button_font = tKFont.Font(family="Arial", size=12, weight="bold")
+title_font = tKFont.Font(
+    family="Courier", 
+    size=46, 
+    weight="bold", 
+    slant="italic"
+)
+button_font = tKFont.Font(
+    family="Arial", 
+    size=25, 
+    weight="bold"
+)
 
 label = tk.Label (window, text="FocusPaw", font=title_font)
-label.pack()
+label.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
 
-start_button = tk.Button(window, text="Start Timer", font=button_font)
-start_button.pack()
+start_button = tk.Button(
+    window, 
+    text="Start Timer", 
+    font=button_font, 
+    width=15, 
+    height=2
+)
+start_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 window.mainloop()
 
