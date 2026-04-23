15aba91142036d19f72b3851417b699a5aa88084
Wong Eng Chee <wong.eng.chee@student.mmu.edu.my>
2026-04-23 23:49:42 +0800

    Delete start timer screen

--- main.py
+++ main.py
@@ -29,11 +29,6 @@ button_font = tKFont.Font(
 label = tk.Label (window, text="FocusPaw", font=title_font)
 label.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
 
-#switch screen
-def show_timer():
-    login_frame.place_forget()
-    timer_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-
 #login frame
 login_frame = tk.Frame(window, width=500, height=500)
 login_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
@@ -48,28 +43,8 @@ login_button = tk.Button(
     font=button_font,
     width=15,
     height=2,
-    command=show_timer
 )
 login_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-
-#timer frame
-timer_frame = tk.Frame(window, width=500, height=500)
-
-timer_title = tk.Label(timer_frame, text="FocusPaw", font=title_font)
-timer_title.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
-
-def show_timer_screen():
-    start_timer(status_label)
-
-start_button = tk.Button(
-    timer_frame, 
-    text="Start Timer",  
-    font=button_font, 
-    width=15, 
-    height=2,
-    command=show_timer_screen
-)
-start_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 window.mainloop()
 
