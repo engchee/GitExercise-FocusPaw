99f1202fa19728d617fc3056fc82ca5785289549
Wong Eng Chee <wong.eng.chee@student.mmu.edu.my>
2026-04-23 12:55:16 +0800

    -

--- main.py
+++ main.py
@@ -59,8 +59,7 @@ timer_title = tk.Label(timer_frame, text="FocusPaw", font=title_font)
 timer_title.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
 
 def show_timer_screen():
-    start_timer()
-    import timer
+    start_timer(status_label)
 
 start_button = tk.Button(
     timer_frame, 
