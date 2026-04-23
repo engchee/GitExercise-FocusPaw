a98225b5034c6d4586cdb4c178bea2906a0a7531
Wong Eng Chee <wong.eng.chee@student.mmu.edu.my>
2026-04-23 12:06:56 +0800

    Fix problem

--- timer.py
+++ timer.py
@@ -14,80 +14,81 @@ def give_up():
     print("User gave up. Deducting HP...(╥‸╥)")
     status_label.config(text="Gave Up", fg="red")
 
-#window setup
-window = tk.Tk()
-window.title("Timer")
+if __name__ == "__main__":
+    #window setup
+    window = tk.Tk()
+    window.title("Timer")
 
-app_width = 500
-app_height = 500
+    app_width = 500
+    app_height = 500
 
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
 
-x = (screen_width - app_width) // 2
-y = (screen_height - app_height) // 2
-window.geometry(f"{app_width}x{app_height}+{x}+{y}")
+    x = (screen_width - app_width) // 2
+    y = (screen_height - app_height) // 2
+    window.geometry(f"{app_width}x{app_height}+{x}+{y}")
 
-window.grid_columnconfigure(0, weight=1)
-window.grid_columnconfigure(1, weight=1)
-window.grid_columnconfigure(2, weight=1)
-window.grid_columnconfigure([0, 1, 2], weight=1)
-window.grid_rowconfigure(2, weight=1)
+    window.grid_columnconfigure(0, weight=1)
+    window.grid_columnconfigure(1, weight=1)
+    window.grid_columnconfigure(2, weight=1)
+    window.grid_columnconfigure([0, 1, 2], weight=1)
+    window.grid_rowconfigure(2, weight=1)
 
-#font style
-button_font = tkFont.Font(
-    family="Consolas",
-    size=12,
-    weight="bold"
-)
+    #font style
+    button_font = tkFont.Font(
+        family="Consolas",
+        size=12,
+        weight="bold"
+    )
 
-title_font = tkFont.Font(
-    family="Courier",
-    size=46,
-    weight="bold",
-)
+    title_font = tkFont.Font(
+        family="Courier",
+        size=46,
+        weight="bold",
+    )
 
-#title and status label
-title_label = tk.Label(
-    window,
-    text="Timer",
-    font=title_font
-)
-title_label.grid(column=1, row=0, pady=(30, 0))
+    #title and status label
+    title_label = tk.Label(
+        window,
+        text="Timer",
+        font=title_font
+    )
+    title_label.grid(column=1, row=0, pady=(30, 0))
 
-status_label = tk.Label(
-    window,
-    text="Are you ready to FOCUS?",
-    font=("Consolas", 14)
-)
-status_label.grid(column=1, row=1, pady=20)
+    status_label = tk.Label(
+        window,
+        text="Are you ready to FOCUS?",
+        font=("Consolas", 14)
+    )
+    status_label.grid(column=1, row=1, pady=20)
 
-#buttons
-btn_start = tk.Button(
-    window,
-    text="Start",
-    font=button_font,
-    width=12,
-    command=start_timer
-)
-btn_start.grid(column=0, row=3, pady=20)
+    #buttons
+    btn_start = tk.Button(
+        window,
+        text="Start",
+        font=button_font,
+        width=12,
+        command=start_timer
+    )
+    btn_start.grid(column=0, row=3, pady=20)
 
-btn_pause = tk.Button(
-    window,
-    text="Pause",
-    font=button_font,
-    width=12,
-    command=pause_timer
-)
-btn_pause.grid(column=1, row=3, pady=20)
+    btn_pause = tk.Button(
+        window,
+        text="Pause",
+        font=button_font,
+        width=12,
+        command=pause_timer
+    )
+    btn_pause.grid(column=1, row=3, pady=20)
 
-btn_give_up = tk.Button(
-    window,
-    text="Give up",
-    font=button_font,
-    width=12,
-    command=give_up
-)
-btn_give_up.grid(column=2, row=3, pady=20)
+    btn_give_up = tk.Button(
+        window,
+        text="Give up",
+        font=button_font,
+        width=12,
+        command=give_up
+    )
+    btn_give_up.grid(column=2, row=3, pady=20)
 
-window.mainloop()
+    window.mainloop()
